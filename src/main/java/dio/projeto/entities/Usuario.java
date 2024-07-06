832f85cd9234414a2decd035435ab53a7d9f8338
Manoel Junior <manuelitojunior@gmail.com>
2024-07-06 19:22:16 -0300

    entidades

---
+++ src/main/java/dio/projeto/entities/Usuario.java
@@ -0,0 +1,42 @@
+package dio.projeto.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private Departamento departamento;
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+}
